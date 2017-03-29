2f3bb1f23bd822eca1437ed2edc33d60f5bc1345
Joseph Tongay <dirk.tongay@gmail.com>
2017-03-28 20:50:08 -0500

    file structure improvements

--- src/app/index.js
+++ src/app/index.js
@@ -1,77 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import {createLogger} from 'redux-logger';
 import { Provider } from 'react-redux';
 
-import App from './containers/App'
-
-const initialState = {
-    result: 1,
-    lastValues: [],
-    userName: "Joey"
-};
-
-
-const mathReducer = (state = {
-    result: 1,
-    lastValues: []
-}, action) => {
-    switch (action.type) {
-        case 'ADD':
-            state = {
-                ...state,
-                result: state.result + action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            };
-            break;
-        case 'SUBTRACT':
-            state = {
-                ...state,
-                result: state.result - action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            };
-            break;
-    }
-    return state;
-}
-
-const userReducer = (state = {
-    name: "Joey",
-    age: 27
-}, action) => {
-    switch (action.type) {
-        case 'SET_NAME':
-            state = {
-                ...state,
-                name: action.payload
-            };
-            break;
-        case 'SET_AGE':
-            state = {
-                ...state,
-                age: action.payload
-            };
-            break;
-    }
-    return state;
-}
-
-const myLogger = (store)=> (next) => (action) =>{
-    console.log("logged action", action);
-    next(action)
-}
-
-const store = createStore(
-    combineReducers(
-        {math: mathReducer, user: userReducer}), 
-        {}, 
-        applyMiddleware(createLogger())
-    );
-
-store.subscribe(()=>{
-    // console.log("store updated!", store.getState());
-});
+import App from './containers/App';
+import store from './store'
 
 render(
     <Provider store={store}>
---
+++ src/app/store.js
@@ -0,0 +1,14 @@
+import logger from 'redux-logger'
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+
+import math from './reducers/mathReducer';
+import user from './reducers/userReducer';
+
+export default createStore(
+    combineReducers({
+        math, 
+        user
+        }), 
+        {}, 
+        applyMiddleware(createLogger())
+    );
